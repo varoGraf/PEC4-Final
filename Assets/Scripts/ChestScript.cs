4608b722358ac37b645738541b0b7d4955810724
Álvaro Grafiá <avox97@gmail.com>
2022-01-12 20:36:11 +0100

    Map and updates

---
+++ Assets/Scripts/ChestScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    public GameObject ammo;
+    private bool isReady = true;
+    private Vector3 positionToSpawn;
+
+
+    void Start()
+    {
+        positionToSpawn = new Vector3(this.transform.position.x + this.GetComponent<BoxCollider2D>().bounds.size.x / 2, this.transform.position.y, this.transform.position.z);
+    }
+    void Update()
+    {
+        if (isReady) { StartCoroutine(SpawnAmmo()); }
+    }
+
+    IEnumerator SpawnAmmo()
+    {
+        yield return new WaitForSeconds(1f);
+        Instantiate(ammo, positionToSpawn, Quaternion.Euler(0f, 0f, 0f));
+        yield return new WaitForSeconds(12f);
+    }
+}
